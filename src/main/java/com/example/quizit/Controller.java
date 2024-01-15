707e2c661582d0309181a33878594122287c1124
annazierlinger <anna.zierlinger@stud.fh-campuswien.ac.at>
2024-01-15 22:55:12 +0100

    Questions Output and Game Starts, but TODO Correct Answer

--- src/main/java/com/example/quizit/Controller.java
+++ src/main/java/com/example/quizit/Controller.java
@@ -9,6 +9,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,12 +20,22 @@ public class Controller {
     private Question currentQuestion;
     private Scene scene;
     private Parent root;
-    boolean firstLoad = true;
-    private int answerID;
+    public String correctAnswer;
+    public String selectedAnswer;
     @FXML
     private Button submit;
     @FXML
     private Button next;
+    @FXML
+    private Label question;
+    @FXML
+    private Button answer1;
+    @FXML
+    private Button answer2;
+    @FXML
+    private Button answer3;
+    @FXML
+    private Button answer4;
 
 
     @FXML
@@ -45,32 +56,58 @@ public class Controller {
         stage.setScene(scene);
         stage.show();
         DataRepository.LiteratureLoader();
-        if (firstLoad) {
             loadNextQuestion(event);
-        }
     }
 
-    public void checkAnswer(ActionEvent event) throws IOException {
+    public void checkAnswer(ActionEvent event) throws IOException {             //TODO: Fix Correct Answer isn't saved
         submit.setVisible(false);
+        System.out.println("Selected Answer: \n" + selectedAnswer);
+        System.out.println("Correct Answer: \n" + correctAnswer);
+        if(answer1.getText() == (correctAnswer)){
+            answer1.setStyle("-fx-background-color: #6DFA76;");
+        }
+        if(answer2.getText() == (correctAnswer)){
+            answer2.setStyle("-fx-background-color: #6DFA76;");
+        }
+        if(answer3.getText() == (correctAnswer)){
+            answer3.setStyle("-fx-background-color: #6DFA76;");
+        }
+        if(answer4.getText() == (correctAnswer)){
+            answer4.setStyle("-fx-background-color: #6DFA76;");
+        }
+        System.out.println("Button outputs:\n" + answer1.getText() + "\n" + answer2.getText() + "\n" + answer3.getText() + "\n" + answer4.getText() );
+
+    }
+
+    public void saveAnswer1(ActionEvent event) throws IOException{
+        selectedAnswer = answer1.getText();
+    }
+
+    public void saveAnswer2(ActionEvent event) throws IOException{
+        selectedAnswer = answer2.getText();
+    }
+    public void saveAnswer3(ActionEvent event) throws IOException{
+        selectedAnswer = answer3.getText();
+    }
+    public void saveAnswer4(ActionEvent event) throws IOException{
+        selectedAnswer = answer4.getText();
     }
 
     public void loadNextQuestion(ActionEvent event) throws IOException {
-        System.out.println(root);
         SecureRandom rand = new SecureRandom();
         int questionID = rand.nextInt(DataRepository.fullPool.size());
         currentQuestion = DataRepository.fullPool.get(questionID);
+        Label questionText = (Label)root.lookup("#question");
+        questionText.setText(currentQuestion.getQuestion());
+        correctAnswer = currentQuestion.getAnswers().get(0);
         int questionSelect;
-
         for (int i = 0; i < 4; i++) {
             questionSelect = rand.nextInt(4 - i);
             Button b = (Button) root.lookup("#answer" + (i + 1));
             b.setText(currentQuestion.getAnswers().get(questionSelect));
             currentQuestion.getAnswers().remove(questionSelect);
         }
-        if (!firstLoad) {
-            switchToCategories(event);
-        } else {
-            firstLoad = false;
-        }
+        DataRepository.fullPool.remove(questionID);
+        System.out.println("LOADNEXTQUESTION CORR ANSWER: \n"+ correctAnswer);
     }
 }
--- src/main/java/com/example/quizit/Data/DataRepository.java
+++ src/main/java/com/example/quizit/Data/DataRepository.java
@@ -41,6 +41,87 @@ public class DataRepository {
         fullPool.add(temp);
 
 
+        temp = new Question();
+        temp.setQuestion("Welches Werk stammt von William Shakespeare?");
+        temp.getAnswers().add("Romeo und Julia");
+        temp.getAnswers().add("Harry Potter");
+        temp.getAnswers().add("Der große Gatsby");
+        temp.getAnswers().add("Krieg und Frieden");
+        fullPool.add(temp);
+
+        temp = new Question();
+        temp.setQuestion("Wer schrieb 'Die Odyssee'?");
+        temp.getAnswers().add("Homer");
+        temp.getAnswers().add("Virgil");
+        temp.getAnswers().add("Dante Alighieri");
+        temp.getAnswers().add("Hermann Hesse");
+        fullPool.add(temp);
+
+        temp = new Question();
+        temp.setQuestion("Welches Buch wurde von Jane Austen geschrieben?");
+        temp.getAnswers().add("Stolz und Vorurteil");
+        temp.getAnswers().add("Der Graf von Monte Christo");
+        temp.getAnswers().add("Faust");
+        temp.getAnswers().add("Anna Karenina");
+        fullPool.add(temp);
+
+        temp = new Question();
+        temp.setQuestion("In welchem Land spielt 'Die Verwandlung' von Franz Kafka?");
+        temp.getAnswers().add("Tschechien");
+        temp.getAnswers().add("Deutschland");
+        temp.getAnswers().add("Österreich");
+        temp.getAnswers().add("Russland");
+        fullPool.add(temp);
+
+        temp = new Question();
+        temp.setQuestion("Wer ist der Autor von '1984'?");
+        temp.getAnswers().add("George Orwell");
+        temp.getAnswers().add("Aldous Huxley");
+        temp.getAnswers().add("Ray Bradbury");
+        temp.getAnswers().add("Philip K. Dick");
+        fullPool.add(temp);
+
+        temp = new Question();
+        temp.setQuestion("Welches Buch wird oft als 'Der Große Gatsby' bezeichnet?");
+        temp.getAnswers().add("F. Scott Fitzgerald");
+        temp.getAnswers().add("Ernest Hemingway");
+        temp.getAnswers().add("Mark Twain");
+        temp.getAnswers().add("J.D. Salinger");
+        fullPool.add(temp);
+
+        temp = new Question();
+        temp.setQuestion("Wer schrieb 'Die Leiden des jungen Werthers'?");
+        temp.getAnswers().add("Johann Wolfgang von Goethe");
+        temp.getAnswers().add("Franz Kafka");
+        temp.getAnswers().add("Thomas Mann");
+        temp.getAnswers().add("Friedrich Schiller");
+        fullPool.add(temp);
+
+        temp = new Question();
+        temp.setQuestion("Was ist das bekannteste Werk von Leo Tolstoi?");
+        temp.getAnswers().add("Krieg und Frieden");
+        temp.getAnswers().add("Anna Karenina");
+        temp.getAnswers().add("Auferstehung");
+        temp.getAnswers().add("Kinderbuch");
+        fullPool.add(temp);
+
+        temp = new Question();
+        temp.setQuestion("Welches Buch erzählt die Geschichte von Pip und Magwitch?");
+        temp.getAnswers().add("Große Erwartungen");
+        temp.getAnswers().add("Sturmhöhe");
+        temp.getAnswers().add("Jane Eyre");
+        temp.getAnswers().add("David Copperfield");
+        fullPool.add(temp);
+
+        temp = new Question();
+        temp.setQuestion("Wer schrieb 'Der kleine Prinz'?");
+        temp.getAnswers().add("Antoine de Saint-Exupéry");
+        temp.getAnswers().add("Jules Verne");
+        temp.getAnswers().add("Hans Christian Andersen");
+        temp.getAnswers().add("Gabriel García Márquez");
+        fullPool.add(temp);
+
+
 
 
     }
